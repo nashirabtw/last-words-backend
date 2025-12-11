5902a311f6b2c837e6fef94f09808578203b106a
Nashira <nashira_o@proton.me>
2025-12-11 13:17:55 -0800

    Add Message model to backend

---
+++ backend/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    message = Column(String, nullable=False)
+    country = Column(String, nullable=True)
+    emoji = Column(String, nullable=True)
